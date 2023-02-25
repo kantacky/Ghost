eafeae8bcda86a560a4cb200db0a7b16bce898a1
Kanta Oikawa <k.oikawa@kantacky.com>
2023-02-25 17:03:15 +0900

    Hoge

--- init-cert.sh
+++ init-cert.sh
@@ -1,1 +1,4 @@
-docker-compose run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos -d www.kantacky.com -d www.chatlab.space -d www.nanosuke.kantacky.com
+docker-compose up -d nginx
+docker-compose run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos -d www.kantacky.com
+docker-compose run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos -d www.chatlab.space
+docker-compose run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos-d www.nanosuke.kantacky.com
