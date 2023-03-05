f32cb2da5ac8b49dbaa6b7f802835198aa150847
Kanta Oikawa <k.oikawa@kantacky.com>
2023-03-05 16:09:23 +0900

    [Update] Update nanosuke.net

--- init-cert.sh
+++ init-cert.sh
@@ -1,4 +1,4 @@
-docker-compose up -d nginx
+docker-compose -f docker-compose.http.yml up -d nginx
 docker-compose run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos -d www.kantacky.com
 docker-compose run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos -d www.chatlab.space
-docker-compose run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos-d www.nanosuke.kantacky.com
+docker-compose run --rm certbot certonly --webroot --webroot-path=/usr/share/nginx/html --email hq@kantacky.com --agree-tos-d www.nanosuke.net
